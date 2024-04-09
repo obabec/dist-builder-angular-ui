7a7162f014250075154de57ffa86fd704740635f
Ondrej Babec <ond.babec@gmail.com>
2024-04-09 09:57:46 +0200

    Add Kubernetes descriptors

--- src/app/services/metadata-http.service.ts
+++ src/app/services/metadata-http.service.ts
@@ -1,24 +1,25 @@
 import {Injectable} from '@angular/core';
 import {MetadataObjectModel} from "../metadataModel/MetadataObjectModel";
 import {HttpClient, HttpErrorResponse, HttpHeaders} from "@angular/common/http";
-
+import {environment} from "../../environments/environment";
 
 @Injectable({
   providedIn: 'root'
 })
 export class MetadataHttpService {
-  url = "http://127.0.0.1:7200"
+  url = environment.apiUrl;
 
   constructor(private http: HttpClient) { }
 
   async getMetadataFromBackend(): Promise<MetadataObjectModel> {
+    console.log("API URL: " + this.url)
     let headers = new HttpHeaders()
       .set('Content-Type', 'application/json')
       .set('Accept', 'application/json')
       .set('Access-Control-Allow-Headers', '*')
       .set('Access-Control-Allow-Origin', '*');
     return new Promise((resolve, reject) => {
-      this.http.get<MetadataObjectModel>(this.url + '/metadata', {'headers': headers})
+      this.http.get<MetadataObjectModel>(environment.apiUrl + '/metadata', {'headers': headers})
         .subscribe({
             next: value => {
               resolve(value);
